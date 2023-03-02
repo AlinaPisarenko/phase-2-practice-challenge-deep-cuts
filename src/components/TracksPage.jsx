168bf488e5ec6bf633a7dc6d9f475643ff5f1fc2
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-03-02 15:49:01 -0500

    first deliverable

--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import AddTrackForm from './AddTrackForm'
 import TracksList from './TracksList'
 
-function TracksPage() {
+URL = 'http://localhost:8001/tracks'
 
+function TracksPage() {
+const [tracks, setTracks] = useState([])
+
+useEffect(() => {
+fetch(URL)
+.then(res => res.json())
+.then(data => setTracks(data))
+}, [])
     
   return (
     <div>
       <Search />
       <AddTrackForm />
-      <TracksList />
+      <TracksList tracks={tracks} />
     </div>
   )
 }
